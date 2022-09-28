7176d353c022cac57492fc26416b206065586461
Amitesh Anand <amitesh.anand@infoaxon.com>
2022-09-28 12:58:59 +0530

    added new script file and updated jenkinsfile for script1

---
+++ iNFoundationLiferayWorkspace/themes/hondathemeupdated.bat
@@ -0,0 +1,18 @@
+@echo off
+echo  > liferay-theme.json
+echo //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- >> site.scss
+echo. >> liferay-theme.json
+echo { >> liferay-theme.json
+echo  "LiferayTheme":{ >> liferay-theme.json
+echo  "LiferayTheme": {">> liferay-theme.json
+echo  "appServerPath": "C:/Liferay/server/tomcat-9.0.37", >> liferay-theme.json
+echo  "deployPath": "C:/Liferay/server/deploy", >> liferay-theme.json
+echo  "url": "http://localhost:8080", >> liferay-theme.json
+echo  "appServerPathPlugin": "C:/Liferay/server/tomcat-9.0.37/webapps/my-space-theme", >> liferay-theme.json
+echo  "deployed": true, >> liferay-theme.json
+echo   "pluginName": "in-control-panel-theme"  >> liferay-theme.json
+echo  } >> liferay-theme.json
+echo  } >> liferay-theme.json
+
+
+
